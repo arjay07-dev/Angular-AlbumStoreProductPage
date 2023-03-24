b17d8f4423494c06140200783f8537ae2dea34e5
arjay07-dev <arjay.cabanela02@gmail.com>
2023-03-25 00:32:47 +0800

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
